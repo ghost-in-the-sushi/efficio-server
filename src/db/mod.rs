4848528dfffb72ce2e3fb5fcf7f05b3ef1d47e63
Geobert Quach <geobert@protonmail.com>
2019-04-04 20:47:51 +0100

    preprod/prod db distinction

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,12 +5,18 @@ pub mod user;
 
 pub use user::store_user;
 
+#[cfg(debug_assertions)]
+const SERVER_ADDR: &str = "redis://127.0.0.1:6379/preprod";
+
+#[cfg(not(debug_assertions))]
+const SERVER_ADDR: &str = "redis://127.0.0.1:6379/prod";
+
 lazy_static! {
   static ref DB_CLIENT: Client = get_client();
 }
 
 fn get_client() -> Client {
-  Client::open("redis://127.0.0.1:6379/").expect("Error while creating redis client.")
+  Client::open(SERVER_ADDR).expect("Error while creating redis client.")
 }
 
 pub fn get_connection() -> redis::RedisResult<Connection> {
