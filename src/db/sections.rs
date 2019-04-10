574ca64b9e6480ec32e86c1d493eb90c79d27e01
Geobert Quach <geobert@protonmail.com>
2019-04-10 20:05:21 +0100

    Use derive-new and derive_deref

---
+++ src/db/sections.rs
@@ -0,0 +1,5 @@
+use redis::{self, Commands, PipelineCommands};
+
+fn section_key()
+
+pub fn 
--- src/types.rs
+++ src/types.rs
@@ -1,38 +1,19 @@
+use derive_deref::Deref;
+use derive_new::new;
 use serde::Serialize;
-use std::ops::Deref;
 
-#[derive(Debug)]
+#[derive(Deref)]
+pub struct Auth<'a>(pub &'a str);
+
+#[derive(Debug, Deref)]
 pub struct UserId(pub u32);
 
-impl Deref for UserId {
-  type Target = u32;
-  fn deref(&self) -> &u32 {
-    &self.0
-  }
-}
-
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, Deref, new)]
 pub struct StoreId {
   store_id: u32,
 }
 
-impl StoreId {
-  pub fn new(id: u32) -> Self {
-    StoreId { store_id: id }
-  }
-}
-
-impl Deref for StoreId {
-  type Target = u32;
-  fn deref(&self) -> &u32 {
-    &self.store_id
-  }
-}
-
-pub struct Auth<'a>(pub &'a str);
-impl<'a> Deref for Auth<'a> {
-  type Target = str;
-  fn deref(&self) -> &str {
-    &self.0
-  }
+#[derive(Serialize, Debug, Deref, new)]
+pub struct SectionId {
+  section_id: u32,
 }
